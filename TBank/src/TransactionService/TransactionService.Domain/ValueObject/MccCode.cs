ac32dc53b877aaf13e8ac4e46cb4d2c6d8bcfe2d
Arseny116 <Olifirov24@gmail.com>
2026-05-16 13:35:00 +0300

    feat: Add VO  MccCode

---
+++ TBank/src/TransactionService/TransactionService.Domain/ValueObject/MccCode.cs
@@ -0,0 +1,16 @@
+﻿namespace TransactionService.Domain.ValueObject
+{
+    public record MccCode
+    {
+        public string Value { get; }
+
+        public MccCode(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value) || value.Length != 4)
+                throw new ArgumentException("MCC код должен состоять из 4 цифр");
+            Value = value;
+        }
+
+        public override string ToString() => Value;
+    }
+}
