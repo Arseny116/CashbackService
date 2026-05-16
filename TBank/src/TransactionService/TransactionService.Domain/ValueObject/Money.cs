204110e8a52bccbcbe96df52f4be4877885a6603
Arseny116 <Olifirov24@gmail.com>
2026-05-16 13:30:16 +0300

    feat: expanded VO money
    add validation field

--- TBank/src/TransactionService/TransactionService.Domain/ValueObject/Money.cs
+++ TBank/src/TransactionService/TransactionService.Domain/ValueObject/Money.cs
@@ -1,5 +1,28 @@
 ﻿namespace TransactionService.Domain.ValueObject
 {
-    // Позиционный синтаксис 
-    public record Money(decimal Amount, Currency currency) { };
+    // Позиционный синтаксис public record Money(decimal Amount, string Currency = "RUB") 
+    public record Money
+    {
+        public decimal Amount { get; }
+        public string Currency { get; }
+
+        public Money(decimal amount, string currency = "RUB")
+        {
+            if (amount < 0)
+                throw new ArgumentException("Сумма не может быть отрицательной");
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public static Money Zero => new(0);
+
+        public Money Add(Money other)
+        {
+            if (Currency != other.Currency)
+                throw new ArgumentException("Нельзя складывать разные валюты");
+            return new Money(Amount + other.Amount, Currency);
+        }
+
+        public override string ToString() => $"{Amount} {Currency}";
+    }
 }
